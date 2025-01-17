e82fc52c6ef9feab751fd2a5bce3ec3e036bcb7e
Roberto Bocio Melo <elpicapica01@gmail.com>
2025-01-17 00:52:47 -0500

    chore: add constant file

---
+++ app/constants.py
@@ -0,0 +1,4 @@
+BASE_URL_AZURE = "https://models.inference.ai.azure.com"
+EMBEDDING_MODEL_NAME = "text-embedding-3-large"
+INDEX_VECTOR_STORE = "langchain-documents-sri-chat"
+INPUT_MODEL_NAME = "gpt-4o-mini"
