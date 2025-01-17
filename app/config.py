a9db7ef97d679d0a47757381f6cf61e8222f7f21
Roberto Bocio Melo <elpicapica01@gmail.com>
2025-01-17 00:52:29 -0500

    fix: configurations types

--- app/config.py
+++ app/config.py
@@ -1,28 +1,21 @@
-from os import getenv
 from pydantic_settings import BaseSettings
+from pydantic import SecretStr
 
 
 class Settings(BaseSettings):
-    OPENAI_API_KEY: str
-    PINECODE_API_KEY: str
-    APP_ENV: str
-    LANGCHAIN_TRACING_V2: bool
-    LANGCHAIN_ENDPOINT: str
-    LANGCHAIN_API_KEY: str
-    LANGCHAIN_PROJECT: str
+    APP_ENV: str = "development"
+    LANGCHAIN_API_KEY: str = "langchain_api_key"
+    LANGCHAIN_ENDPOINT: str = "langchain_endpoint"
+    LANGCHAIN_PROJECT: str = "langchain_project"
+    LANGCHAIN_TRACING_V2: bool = False
+    OPENAI_API_KEY_GH: SecretStr = SecretStr("openai_api_key_gh")
+    OPENAI_API_KEY: str = "openai_api_key"
+    PINECODE_API_KEY: str = "pinecode_api_key"
 
     class Config:
+        case_sensitive = True
         env_file = ".env"
         env_file_encoding = "utf-8"
-        case_sensitive = True
 
 
-settings = Settings(
-    APP_ENV=getenv("APP_ENV", "development"),
-    OPENAI_API_KEY=getenv("OPENAI_API_KEY", ""),
-    PINECODE_API_KEY=getenv("PINECODE_API_KEY", ""),
-    LANGCHAIN_TRACING_V2=(getenv("LANGCHAIN_TRACING_V2", "false").lower() == "true"),
-    LANGCHAIN_ENDPOINT=getenv("LANGCHAIN_ENDPOINT", ""),
-    LANGCHAIN_API_KEY=getenv("LANGCHAIN_API_KEY", ""),
-    LANGCHAIN_PROJECT=getenv("LANGCHAIN_PROJECT", ""),
-)
+settings = Settings()
