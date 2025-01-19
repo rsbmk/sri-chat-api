f9826dfec40f712cd9d469048b790c316e136331
Roberto Bocio Melo <elpicapica01@gmail.com>
2025-01-18 21:23:09 -0500

    feat: add cors

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.routes import files, chats
 
 app = FastAPI()
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:3000"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 api_version_v1 = "v1"
 prefix = f"/api/{api_version_v1}"
