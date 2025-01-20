b86943892aa0c1f0e246f2f744d6c62bd23e6133
Roberto Bocio Melo <elpicapica01@gmail.com>
2025-01-20 17:27:03 -0500

    docs: add docs strings

--- app/core/config.py
+++ app/core/config.py
@@ -1,8 +1,21 @@
+"""
+Configuration module for application settings.
+
+This module defines and loads environment variables and application settings
+using Pydantic's BaseSettings.
+"""
+
 from pydantic_settings import BaseSettings
 from pydantic import SecretStr
 
 
 class Settings(BaseSettings):
+    """
+    Settings class that defines and loads application configuration.
+
+    Environment variables and default values are defined here using Pydantic's BaseSettings.
+    """
+
     APP_ENV: str = "development"
     LANGCHAIN_API_KEY: str = "langchain_api_key"
     LANGCHAIN_ENDPOINT: str = "langchain_endpoint"
@@ -13,6 +26,11 @@ class Settings(BaseSettings):
     PINECODE_API_KEY: str = "pinecode_api_key"
 
     class Config:
+        """
+        Configuration for the Settings class, including case sensitivity and
+        .env file handling.
+        """
+
         case_sensitive = True
         env_file = ".env"
         env_file_encoding = "utf-8"
